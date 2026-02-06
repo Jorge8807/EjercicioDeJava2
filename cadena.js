381dbf0a762d3c71d22dd82d124d2690dc04a675
Jorge Luis <suarce.jorgelu@gmail.com>
2026-02-05 23:28:47 -0600

    se agregaron funciones de texto

--- cadena.js
+++ cadena.js
@@ -5,3 +5,15 @@ function concatenarCadenas(){
   nombreCompleto=nombre+ " " +apellidoPaterno+ " " +apellidoMaterno;
   document.getElementById("cajaNombreCompleto").value=nombreCompleto;
 }
+
+function longitudTexto(texto){
+  return texto.length;
+}
+
+function textoAMayusculas(texto){
+  return texto.toUpperCase();
+}
+
+function textoAMinusculas(texto){
+  return texto.toLowerCase();
+}
